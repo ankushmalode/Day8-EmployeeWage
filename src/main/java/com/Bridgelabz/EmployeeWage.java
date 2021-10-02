59627ec9f7f8e24dfc6f94de0bf23022ad423ae6
ankushmalode <you@example.com>
2021-10-02 14:13:15 +0530

    Calculating Wages for a Month

--- src/main/java/com/Bridgelabz/EmployeeWage.java
+++ src/main/java/com/Bridgelabz/EmployeeWage.java
@@ -11,27 +11,30 @@ public class EmployeeWage {
     public static void main(String[] args) {
         WageCalculate();
     }
-    static void WageCalculate (){
-        int IS_PRESENT=1,PartTimePresent=2;
-        Random num=new Random();
-        int Attendance= num.nextInt(3);
-        if (Attendance==IS_PRESENT)
-            System.out.println("Employee is Present");
-        else if (Attendance==PartTimePresent)
-            System.out.println("Employee is Present but for Part Time");
-        else
-            System.out.println("Employee is Absent");
-        int WagePerHr=20, FullDayHr=8, TotalHr=0, PartTimeHr=4;
-        switch (Attendance){
-            case 1:
-                TotalHr+=FullDayHr;
-                break;
-            case 2:
-                TotalHr+=PartTimeHr;
-                break;
-            default:
-                TotalHr+=0;
+
+    static void WageCalculate() {
+        int IS_PRESENT = 1, PartTimePresent = 2, WagePerHr = 20, FullDayHr = 8, TotalHr = 0, PartTimeHr = 4, DayPerMonth = 20;
+        for (int i = 0; i <= DayPerMonth; i++) {
+            Random num = new Random();
+            int Attendance = num.nextInt(3);
+            if (Attendance == IS_PRESENT)
+                System.out.println("Employee is Present");
+            else if (Attendance == PartTimePresent)
+                System.out.println("Employee is Present but for Part Time");
+            else
+                System.out.println("Employee is Absent");
+
+            switch (Attendance) {
+                case 1:
+                    TotalHr += FullDayHr;
+                    break;
+                case 2:
+                    TotalHr += PartTimeHr;
+                    break;
+                default:
+                    TotalHr += 0;
+            }
         }
-        System.out.println("Daily Employee Wage is: "+TotalHr);
+        System.out.println("Daily Employee Wage is: " + TotalHr);
     }
 }
